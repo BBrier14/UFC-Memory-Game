cc4af009652b2ba3c2af39d97cf952fb43f594ba
Benjamin Brier <ben.brier14@gmail.com>
2020-01-03 11:10:52 -0500

    score display code

---
+++ clicky/src/components/main/score.js
@@ -0,0 +1,12 @@
+//----- Import -----//
+import React from 'react';
+
+//----- Score Prop Create -----//
+const Score = (props) => (
+    <div className="score=keeper">
+        Score: {props.score}
+    </div>
+);
+
+//----- Export -----//
+export default Score
