84e43bf88ae0691c1321df8bb87725086973ab65
Benjamin Brier <ben.brier14@gmail.com>
2020-01-03 11:50:50 -0500

    header code

---
+++ clicky/src/components/header/header.js
@@ -0,0 +1,13 @@
+//----- Import -----//
+import React from 'react';
+
+//---- Header Component -----//
+const Header = () => {
+    return(
+        <header className="App-header">
+            <h2>UFC Fighter Clicky Game</h2>
+        </header>
+    )
+};
+
+export default Header;
