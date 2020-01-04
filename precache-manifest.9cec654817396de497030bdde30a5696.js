12f12e10a7f3d9fb399f894459dbfd448469ce0e
Benjamin Brier <ben.brier14@gmail.com>
2020-01-04 11:56:03 -0500

    Updates

---
+++ precache-manifest.9cec654817396de497030bdde30a5696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4d96a05c5fb91b3b91901838c3909b",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "523af5e768b13352d53f",
+    "url": "/clickygame/static/css/main.1a454fe7.chunk.css"
+  },
+  {
+    "revision": "730378a644dfab8aff52",
+    "url": "/clickygame/static/js/2.fd7e5312.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.fd7e5312.chunk.js.LICENSE"
+  },
+  {
+    "revision": "523af5e768b13352d53f",
+    "url": "/clickygame/static/js/main.c5acba01.chunk.js"
+  },
+  {
+    "revision": "efc13ec2a04ad6f1f191",
+    "url": "/clickygame/static/js/runtime-main.5e463b13.js"
+  }
+]);
